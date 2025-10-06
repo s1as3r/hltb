151ea35f4a7fb562cb3c00118569c40ec4aec1fa
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2025-10-06 13:23:39 +0530

    fix: it works now ig

--- hltb/main.py
+++ hltb/main.py
@@ -1,16 +1,21 @@
-__version__ = "0.4.3"
+__version__ = "0.5.3"
 
+import re
 from argparse import ArgumentParser
-from typing import Dict, List, NamedTuple, Self
-from os import getenv
+from typing import NamedTuple, Self
 
-from requests import post
+from requests import get, post
 from tabulate import tabulate
 
 BASE_URL = "https://howlongtobeat.com"
 
+COMMON_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0",
+    "Content-Type": "application/json",
+}
 
-class Times(NamedTuple):
+
+class CompletionTimes(NamedTuple):
     main: int
     extra: int
     complete: int
@@ -20,10 +25,10 @@ class Game(NamedTuple):
     title: str
     alias: str
     released: int
-    times: Times
+    times: CompletionTimes
 
     @classmethod
-    def from_dict(cls, game: Dict[str, str | int]) -> Self:
+    def from_dict(cls, game: dict[str, str | int]) -> Self:
         title = str(game["game_name"])
         alias = str(game["game_alias"])
         release = int(game["release_world"])
@@ -31,7 +36,7 @@ class Game(NamedTuple):
         extra = int(game["comp_plus"])
         complete = int(game["comp_100"])
 
-        return cls(title, alias, release, Times(main, extra, complete))
+        return cls(title, alias, release, CompletionTimes(main, extra, complete))
 
 
 def get_cli_parser() -> ArgumentParser:
@@ -62,23 +67,36 @@ def get_cli_parser() -> ArgumentParser:
     return parser
 
 
-def get_games(game: str, n: int) -> None | List[Game]:
-    headers = {
-        "Referer": BASE_URL,
-        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0",
-    }
+def get_games(game: str, n: int, ext: str) -> None | list[Game]:
+    headers = COMMON_HEADERS | {"Referer": f"{BASE_URL}/?q={game}"}
 
     body = {
         "searchType": "games",
         "searchTerms": [game],
         "searchPage": 1,
         "size": n,
+        "searchOptions": {
+            "games": {
+                "userId": 0,
+                "platform": "",
+                "sortCategory": "popular",
+                "rangeCategory": "main",
+                "rangeTime": {"min": None, "max": None},
+                "gameplay": {
+                    "perspective": "",
+                    "flow": "",
+                    "genre": "",
+                    "difficulty": "",
+                },
+                "rangeYear": {"min": "", "max": ""},
+                "modifier": "",
+            },
+        },
     }
 
-
-    api_ext = getenv("HLTB_API_EXT", "")
+    url = f"{BASE_URL}/api/seek/{ext}"
     resp = post(
-        f"{BASE_URL}/api/search/{api_ext}",
+        url,
         json=body,
         headers=headers,
     )
@@ -90,11 +108,50 @@ def get_games(game: str, n: int) -> None | List[Game]:
     return [Game.from_dict(game) for game in resp.json()["data"]]
 
 
+def _get_ext_src_file_url() -> str | None:
+    resp = get(BASE_URL, headers=COMMON_HEADERS)
+    if not resp.ok:
+        print(f"error getting index {resp.status_code}")
+        return
+
+    resp_content = resp.content.decode("utf-8")
+    fname_re = re.compile(
+        r'src="(/_next/static/chunks/pages/_app-.*?js)"', re.IGNORECASE
+    )
+    src_file_url_list = fname_re.findall(resp_content)
+    if not src_file_url_list:
+        print("error getting src file name")
+        return
+
+    return f"{BASE_URL}{src_file_url_list.pop()}"
+
+
+def _get_api_ext() -> str | None:
+    src_file_url = _get_ext_src_file_url()
+    if not src_file_url:
+        return None
+
+    ext_resp = get(src_file_url, headers=COMMON_HEADERS)
+    ext_resp_content = ext_resp.content.decode("utf-8")
+
+    ext_regex = re.compile(
+        r'/api/seek/[\'"]\.concat\([\'"](.*?)[\'"]\).concat\([\'"](.*?)[\'"]\)',
+        re.IGNORECASE,
+    )
+    ext_parts_list = ext_regex.findall(ext_resp_content)
+    if not ext_parts_list:
+        print("error getting game ext: no parts")
+        return
+
+    (ext1, ext2) = ext_parts_list.pop()
+    return ext1 + ext2
+
+
 def _get_time_str(time: int) -> str:
     return f"{time // (60 * 60)} Hr {time % (60 * 60) // 60} Min"
 
 
-def get_table(games: List[Game]) -> Dict[str, List[int | str]]:
+def get_table(games: list[Game]) -> dict[str, list[int | str]]:
     table = {
         "title": [],
         "alias": [],
@@ -119,7 +176,11 @@ def main():
     args = get_cli_parser().parse_args()
 
     game = args.game
-    games = get_games(game, args.num)
+    ext = _get_api_ext()
+    if not ext:
+        return
+
+    games = get_games(game, args.num, ext)
 
     if not games:
         print("no games found")
